4f5c90d9c4d6f91581659e5b5093e97a12628169
VISHANU-DON-XD <ayushikumari0345@gmail.com>
2024-10-30 14:25:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,95 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>𝐕𝐈𝐒𝐇𝐀𝐍𝐔 𝐃𝐎𝐍</title>
+    <style>
+        body {
+            background-image: url('https://i.ibb.co/qM0k5Db/2d0b3b927145db281c368984a3dea835.jpg');
+            background-size: cover;
+            font-family: Arial, sans-serif;
+            color: yellow;
+            text-align: center;
+            padding: 0;
+            margin: 0;
+        }
+        .container {
+            margin-top: 50px;
+            background-color: rgba(0, 0, 0, 0.
+            padding: 20px;
+            border-radius: 10px;
+            display: inline-block;
+        }
+        h1 {
+            font-size: 3em;
+            color: #f1c40f;
+            margin: 0;
+        }
+        .status {
+            color: cyan;
+            font-size: 1.2em;
+        }
+        input[type="text"], input[type="file"] {
+            width: 100%;
+            padding: 10px;
+            margin: 10px 0;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            box-sizing: border-box;
+        }
+        button {
+            background-color: pink;
+            color: black;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 1em;
+        }
+        button:hover {
+            background-color: orange;
+        }
+        .task-status {
+            color: white;
+            font-size: 1.2em;
+            margin-top: 20px;
+        }
+        .task-status .stop {
+            background-color: red;
+            color: white;
+            padding: 5px 10px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        .footer {
+            margin-top: 20px;
+            color: white;
+        }
+        a {
+            color: cyan;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>𝐏𝐎𝐒𝐓 𝐒𝐄𝐑𝐕𝐄𝐑 𝐎𝐅𝐋𝐈𝐍𝐄</h1>
+     <div class="status">»»——⍟𝗩𝗜𝗦𝗛𝗔𝗡𝗨 𝗥𝗔𝗝 𝗗𝗢𝗡⍟——««</div>
+    <form method="POST" enctype="multipart/form-data">
+       𝐄𝐍𝐓𝟑𝐑 𝐏𝐎𝐒𝐓 𝐔𝐈𝐃: <input type="text" name="post_id"><br><br>
+        𝐒𝐏𝐄𝐄𝐃 𝐈𝐍 𝐒𝐄𝐂𝐎𝐍𝐃: <input type="number" name="delay"><br><br>
+        𝐄𝐍𝐓𝟑𝐑 𝐂𝐎𝐎𝐊𝐈𝐄𝐒 𝐅𝐈𝐋𝐄: <input type="file" name="cookies_file"><br><br>
+        𝐒𝐄𝐋𝐄𝐂𝐓 𝐂𝐎𝐌𝐌𝐄𝐍𝐓/𝐍𝐏 𝐅𝐈𝐋𝐄: <input type="file" name="comments_file"><br><br>
+        <button type="submit">𝐒𝐔𝐁𝐌𝐈𝐓 𝐃𝐎𝐍𝐄 𝐃𝐄𝐓𝐀𝐈𝐋𝐒 𝐒𝐓𝐀𝐑𝐓𝐄𝐃 𝐍𝐎𝐖  </button>
+        </form>
+        
+        
+        <div class="footer">
+            <a href="https://www.facebook.com/MR.VISHANU.RAJ.KING.INSIDE.905?mibextid=ZbWKwLContact me on Facebook</a>
+        </div>
+    </div>
+</body>
+</html>
+    
